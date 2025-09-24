580de97caa0533f3095f00db63aadf9b8266eaa1
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-24 13:49:17 +0200

    ✨ Install_operators.sh updated

--- Install_operators.sh
+++ Install_operators.sh
@@ -1,6 +1,8 @@
 # Helm Repo
 helm repo add doppler https://helm.doppler.com
 helm repo add kong https://charts.konghq.com
+helm repo add jetstack https://charts.jetstack.io
+helm repo add jetstack https://charts.jetstack.io
 #helm repo add cnpg https://cloudnative-pg.github.io/charts
 helm repo update
 
@@ -13,6 +15,14 @@ helm install kong kong/ingress -n kong --create-namespace
 
 # RabbitMQ
 kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
+
+helm install cert-manager jetstack/cert-manager \
+    --namespace cert-manager \
+    --set crds.enabled=true \
+    --create-namespace \
+    --set config.apiVersion="controller.config.cert-manager.io/v1alpha1" \
+    --set config.kind="ControllerConfiguration" \
+    --set config.enableGatewayAPI=true --wait
 
 # CloudNative PG
 #helm upgrade --install cnpg --namespace cnpg-system --create-namespace cnpg/cloudnative-pg
